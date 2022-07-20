04a671ee62c305d806cfb3ea0e7bd5efa575f4a9
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2022-07-20 05:26:07 +0200

    WasmDemo project files

---
+++ TradingView.Blazor.WasmDemo/Data/ChartService.cs
@@ -0,0 +1,24 @@
+﻿namespace TradingView.Blazor.WasmDemo.Data;
+public class ChartService
+{
+    public List<Ohlcv> GetSmallSampleData()
+    {
+        using (var reader = new StreamReader("/sample-data/small-sample-data.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            return csv.GetRecords<Ohlcv>().ToList();
+    }
+    
+    public List<Ohlcv> GetSampleData()
+    {
+        using (var reader = new StreamReader("/sample-data/sample-data.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                return csv.GetRecords<Ohlcv>().ToList();
+    }
+    
+    public List<Marker> GetSampleMarkers()
+    {
+        using (var reader = new StreamReader("/sample-data/sample-markers.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                return csv.GetRecords<Marker>().ToList();
+    }
+}
---
+++ TradingView.Blazor.WasmDemo/Usings.cs
@@ -0,0 +1,17 @@
+﻿// .NET
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using System;
+global using System.Diagnostics;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.IO;
+global using System.Linq;
+global using System.Threading.Tasks;
+
+// Packages
+global using CsvHelper;
+
+// Tradingview.Blazor
+global using TradingView.Blazor.WasmDemo.Data;
+global using TradingView.Blazor.Models;
